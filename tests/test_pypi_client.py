83c0bd422d35c9bdc679fd8ef3105d0eae4d8aaf
catile <ohmycatile@gmail.com>
2025-12-06 11:36:34 +0300

    refactor: now test passes with convenient types

--- tests/test_pypi_client.py
+++ tests/test_pypi_client.py
@@ -2,6 +2,7 @@ from datetime import datetime, UTC
 
 import pytest
 from reqresolve.pypi.client import PypiClient
+from reqresolve.pypi.exception import NoSuitableVersionException, PackageNotFoundException
 
 
 @pytest.mark.asyncio
@@ -13,13 +14,13 @@ async def test_existing_package():
 
 @pytest.mark.asyncio
 async def test_non_existing_package():
-    with pytest.raises(Exception):
+    with pytest.raises(PackageNotFoundException):
         client = PypiClient(datetime.now(UTC))
         await client.query_packages(['-1'])
 
 
 @pytest.mark.asyncio
 async def test_no_matching_versions():
-    with pytest.raises(Exception):
+    with pytest.raises(NoSuitableVersionException):
         client = PypiClient(datetime(1900, 1, 1).astimezone(UTC))
         await client.query_packages(['numpy'])
